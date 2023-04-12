dad385761f9948b79c75b213c856c6c50e6cbbfc
Adalto Selau Sparremberger <assparremberger@senacrs.com.br>
2023-04-12 11:34:44 -0300

    Aula06 - 12/04/2023 - Polimorfismo

---
+++ Aula06-Polimorfismo/Juridica.py
@@ -0,0 +1,31 @@
+from Pessoa import Pessoa
+from Fisica import Fisica
+
+class Juridica(Pessoa):
+
+    def __init__(self, nome, fone, cidade, cnpj):
+        super().__init__(nome, fone, cidade)
+        self.cnpj = cnpj
+        self.qtd_funcionarios = 0
+        self.funcionarios = []
+
+    def addFuncionario(self, funcionario):
+        self.funcionarios.append( funcionario )
+        self.qtd_funcionarios += 1
+
+    def imprimir(self):
+        print("Empresa: ", self.nome)
+        print("Fone: ", self.fone)
+        print("Cidade: ", self.cidade.nome)
+#        super().imprimir()
+        print("Qtd Funcionários: ", self.qtd_funcionarios)
+        print("Funcionários: \n---------------------")
+        if len( self.funcionarios ) > 0:
+            for func in self.funcionarios:
+                print("Nome Func.: ", func.nome)
+                print("Fone Func.: ", func.fone)
+                print("Cidade Func.: ", func.cidade.nome)
+                print(" ------------------ ")
+
+
+
